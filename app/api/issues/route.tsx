16a488599fcceeea56c5d9fd79340f824e851257
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-07-17 02:30:13 +0100

    implement client side validation

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,25 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import { createIssueSchemas } from "../../validationSchemas";
 
 const prisma = new PrismaClient();
 
 const createIssueSchema = z.object({
-  title: z.string().min(1, "Title is required.").max(255),
-  description: z.string().min(1, "Description is required."),
+  title: z.string().min(1).max(255),
+  description: z.string().min(1).max(255),
 });
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
-
+  
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
-
+  
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
   });
-
+  
   return NextResponse.json(newIssue, { status: 201 });
 }
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,27 @@
 "use client";
 import { useState } from "react";
-import { TextField, Button, Callout } from "@radix-ui/themes";
+import { TextField, Button, Callout, Text } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchemas } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchemas>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchemas),
+  });
   const [error, setError] = useState("");
   return (
     <div className="max-w-xl">
@@ -38,6 +45,11 @@ const NewIssuePage = () => {
           placeholder="Title"
           {...register("title")}
         ></TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -45,6 +57,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
