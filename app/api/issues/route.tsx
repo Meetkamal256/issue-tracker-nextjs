915b35f2dc72e0bd27e2f507e430876031874f8d
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-07-17 01:13:28 +0100

    handled errors in form

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -5,14 +5,14 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1).max(255),
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required."),
 });
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
-  
+
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
