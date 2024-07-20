82f0f7f151e8e71b6d952213a4b273b4ec88d045
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-07-20 03:28:51 +0100

    added loading skeletons for new issue page and issue details page

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Heading, Card } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <div className="max-w-xl">
+      <Skeleton />
+      <div className="flex space-x-3 my-2 items-center">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </div>
+      <Card className="prose" mt="4">
+        <Skeleton count={5} />
+      </Card>
+    </div>
+  );
+};
 
-export default LoadingIssueDetailPage
+export default LoadingIssueDetailPage;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/prisma/client";
 import { Card, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import delay from "delay";
 
 interface Props {
   params: { id: string };
@@ -13,6 +14,7 @@ const IssueDetailPage = async ({ params }: Props) => {
     where: { id: parseInt(params.id) },
   });
   if (!issue) notFound();
+  await delay(3000);
   return (
     <div>
       <Heading as="h2">{issue.title}</Heading>
